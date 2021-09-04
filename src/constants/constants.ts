c0e1f093bc34b499755253c88a3b7203813d75bd
Girish Parate <girishparate41@gmail.com>
2021-09-04 14:24:54 +0530

    small fix

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,9 +1,9 @@
-export const AppConst = {
-  URL: 'https://api.unsplash.com/',
-  CLIENT_ID:
-    '?client_id=jRBzm2zUw2eoIPSHZxLvY_hnSh0P8J91P2THDay4y8w',
-  PHOTOS: 'photos',
-  TOPICS: 'topics',
-  SEARCH: 'search',
-  THEME: 'THEME',
+export enum AppConst {
+  URL = 'https://api.unsplash.com/',
+  CLIENT_ID =
+  '?client_id=jRBzm2zUw2eoIPSHZxLvY_hnSh0P8J91P2THDay4y8w',
+  PHOTOS = 'photos',
+  TOPICS = 'topics',
+  SEARCH = 'search',
+  THEME = 'THEME',
 }
